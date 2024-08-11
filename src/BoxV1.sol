e33bfa12e6d10c6a288b3cf980132f30486a74c9
Vivek <vivek.mitra@outlook.com>
2024-08-11 10:37:38 +1000

    openzeppelin contracts imported and applied

---
+++ src/BoxV1.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 internal number;
+
+    // @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function getVersion() external pure returns (uint256) {
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {} //leaving this blank as we don't any authorisation stuff
+        //check if msg.sender == owner
+}
